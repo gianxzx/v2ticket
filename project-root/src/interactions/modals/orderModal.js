93bd2c07cc3a2588838f2ebac402ab474a784ce5
gianxzx <tulanagian6@gmail.com>
2026-02-04 23:58:17 +0800

    Update orderModal.js

--- project-root/src/interactions/modals/orderModal.js
+++ project-root/src/interactions/modals/orderModal.js
@@ -1,1 +1,29 @@
+const createTicketChannel = require("../../tickets/createChannel");
+const { createTicket } = require("../../database/queries/tickets");
+const ticketEmbed = require("../../embeds/ticket");
 
+module.exports = async (interaction) => {
+  const order = interaction.fields.getTextInputValue("order_item");
+
+  await interaction.deferReply({ ephemeral: true });
+
+  const channel = await createTicketChannel(interaction);
+
+  await createTicket({
+    guild_id: interaction.guild.id,
+    channel_id: channel.id,
+    customer_id: interaction.user.id,
+    order_data: { order }
+  });
+
+  await channel.send(
+    ticketEmbed({
+      customerId: interaction.user.id,
+      order
+    })
+  );
+
+  await interaction.editReply({
+    content: `✅ Your ticket has been created: ${channel}`
+  });
+};
