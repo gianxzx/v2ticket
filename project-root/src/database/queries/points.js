23de057dfe716dafedd8315d9cac0bee89bcb4ee
gianxzx <tulanagian6@gmail.com>
2026-02-05 00:02:36 +0800

    Update points.js

--- project-root/src/database/queries/points.js
+++ project-root/src/database/queries/points.js
@@ -1,1 +1,13 @@
+const pool = require("../pool");
 
+async function addPoint(guild_id, user_id) {
+  await pool.query(
+    `INSERT INTO points (guild_id, user_id, points)
+     VALUES ($1,$2,1)
+     ON CONFLICT (guild_id, user_id)
+     DO UPDATE SET points = points.points + 1`,
+    [guild_id, user_id]
+  );
+}
+
+module.exports = { addPoint };
