358cb14ffe66758d600d1be7f8f6075805db2e35
gianxzx <tulanagian6@gmail.com>
2026-02-05 00:07:08 +0800

    Update discounts.js

--- project-root/src/database/queries/discounts.js
+++ project-root/src/database/queries/discounts.js
@@ -1,1 +1,24 @@
+const pool = require("../pool");
 
+// Get discount for a user
+async function getDiscount(guild_id, user_id) {
+  const res = await pool.query(
+    `SELECT discount_percent FROM discounts
+     WHERE guild_id = $1 AND user_id = $2`,
+    [guild_id, user_id]
+  );
+  return res.rows[0]?.discount_percent || 0;
+}
+
+// Set or update discount for a user
+async function setDiscount(guild_id, user_id, percent) {
+  await pool.query(
+    `INSERT INTO discounts (guild_id, user_id, discount_percent)
+     VALUES ($1, $2, $3)
+     ON CONFLICT (guild_id, user_id)
+     DO UPDATE SET discount_percent = EXCLUDED.discount_percent`,
+    [guild_id, user_id, percent]
+  );
+}
+
+module.exports = { getDiscount, setDiscount };
