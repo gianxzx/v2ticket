da9e46d3af48f1ad5ae75757ad6b62553ff69596
gianxzx <tulanagian6@gmail.com>
2026-02-04 23:54:08 +0800

    Update ticket.js

--- project-root/src/embeds/ticket.js
+++ project-root/src/embeds/ticket.js
@@ -1,1 +1,29 @@
+const {
+  EmbedBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle
+} = require("discord.js");
 
+module.exports = function ticketPanelEmbed() {
+  const embed = new EmbedBuilder()
+    .setTitle("🍽️ Order Ticket")
+    .setDescription(
+      "Click the button below to place your order.\n" +
+      "A private ticket will be created for you."
+    )
+    .setColor(0x3498db)
+    .setFooter({ text: "Ticket System" });
+
+  const row = new ActionRowBuilder().addComponents(
+    new ButtonBuilder()
+      .setCustomId("open_ticket")
+      .setLabel("Open Ticket")
+      .setStyle(ButtonStyle.Primary)
+  );
+
+  return {
+    embeds: [embed],
+    components: [row]
+  };
+};
