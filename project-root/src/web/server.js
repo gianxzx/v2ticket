468daf775b7e729242772d3935ea499a5be97fc9
gianxzx <tulanagian6@gmail.com>
2026-02-04 23:01:27 +0800

    Update server.js

--- project-root/src/web/server.js
+++ project-root/src/web/server.js
@@ -1,1 +1,13 @@
+const express = require("express");
+const app = express();
 
+app.get("/", (_, res) => {
+  res.send("Bot is alive");
+});
+
+module.exports = function startWeb() {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () =>
+    console.log(`[WEB] Listening on ${PORT}`)
+  );
+};
