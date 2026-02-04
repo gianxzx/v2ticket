c65096aeac2366d0ae39e0c2b8d49473c1872645
gianxzx <tulanagian6@gmail.com>
2026-02-04 23:55:32 +0800

    Update openTicket.js

--- project-root/src/interactions/buttons/openTicket.js
+++ project-root/src/interactions/buttons/openTicket.js
@@ -1,1 +1,24 @@
+const {
+  ModalBuilder,
+  TextInputBuilder,
+  ActionRowBuilder,
+  TextInputStyle
+} = require("discord.js");
 
+module.exports = async (interaction) => {
+  const modal = new ModalBuilder()
+    .setCustomId("order_modal")
+    .setTitle("Order Details");
+
+  const item = new TextInputBuilder()
+    .setCustomId("order_item")
+    .setLabel("What would you like to order?")
+    .setStyle(TextInputStyle.Short)
+    .setRequired(true);
+
+  modal.addComponents(
+    new ActionRowBuilder().addComponents(item)
+  );
+
+  await interaction.showModal(modal);
+};
