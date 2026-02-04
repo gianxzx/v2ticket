f1b744d463154d958d3b921b726a1cad216753a4
gianxzx <tulanagian6@gmail.com>
2026-02-05 00:02:57 +0800

    Update transcript.js

--- project-root/src/tickets/transcript.js
+++ project-root/src/tickets/transcript.js
@@ -1,1 +1,9 @@
+const { createTranscript } = require("discord-html-transcripts");
 
+module.exports = async function generateTranscript(channel) {
+  return await createTranscript(channel, {
+    limit: -1,
+    returnType: "attachment",
+    filename: `transcript-${channel.id}.html`
+  });
+};
