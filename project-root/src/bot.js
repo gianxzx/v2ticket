cc65a766c25d524fab17e95c1758fedc7b9aac68
gianxzx <tulanagian6@gmail.com>
2026-02-04 23:03:17 +0800

    Update bot.js

--- project-root/src/bot.js
+++ project-root/src/bot.js
@@ -1,1 +1,32 @@
+const {
+  Client,
+  GatewayIntentBits,
+  Partials,
+  Collection
+} = require("discord.js");
 
+const commandHandler = require("./handlers/commandHandler");
+const interactionHandler = require("./handlers/interactionHandler");
+
+module.exports = async function startBot() {
+  const client = new Client({
+    intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.MessageContent,
+      GatewayIntentBits.DirectMessages
+    ],
+    partials: [Partials.Channel]
+  });
+
+  client.commands = new Collection();
+
+  client.once("ready", () => {
+    console.log(`[BOT] Logged in as ${client.user.tag}`);
+  });
+
+  commandHandler(client);
+  interactionHandler(client);
+
+  await client.login(process.env.DISCORD_TOKEN);
+};
