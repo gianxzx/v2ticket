8d603cea77146fc614d65ae925b6046dc1e8b094
gianxzx <tulanagian6@gmail.com>
2026-02-04 23:56:55 +0800

    Update permissions.js

--- project-root/src/tickets/permissions.js
+++ project-root/src/tickets/permissions.js
@@ -1,1 +1,30 @@
+const { PermissionsBitField } = require("discord.js");
 
+module.exports = function buildTicketPermissions({
+  guild,
+  customerId,
+  chefRoleId
+}) {
+  return [
+    {
+      id: guild.roles.everyone.id,
+      deny: [PermissionsBitField.Flags.ViewChannel]
+    },
+    {
+      id: customerId,
+      allow: [
+        PermissionsBitField.Flags.ViewChannel,
+        PermissionsBitField.Flags.SendMessages,
+        PermissionsBitField.Flags.ReadMessageHistory
+      ]
+    },
+    {
+      id: chefRoleId,
+      allow: [
+        PermissionsBitField.Flags.ViewChannel,
+        PermissionsBitField.Flags.SendMessages,
+        PermissionsBitField.Flags.ReadMessageHistory
+      ]
+    }
+  ];
+};
