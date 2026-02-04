b4885dcff291df15c5303a77a9d72a3ddfe83f1b
gianxzx <tulanagian6@gmail.com>
2026-02-04 23:56:13 +0800

    Update tickets.js

--- project-root/src/database/queries/tickets.js
+++ project-root/src/database/queries/tickets.js
@@ -1,1 +1,36 @@
+const pool = require("../pool");
 
+async function createTicket({
+  guild_id,
+  channel_id,
+  customer_id,
+  order_data
+}) {
+  const res = await pool.query(
+    `INSERT INTO tickets (
+      guild_id,
+      channel_id,
+      customer_id,
+      status,
+      order_data
+    )
+    VALUES ($1,$2,$3,'open',$4)
+    RETURNING *`,
+    [guild_id, channel_id, customer_id, order_data]
+  );
+
+  return res.rows[0];
+}
+
+async function getTicketByChannel(channel_id) {
+  const res = await pool.query(
+    `SELECT * FROM tickets WHERE channel_id = $1`,
+    [channel_id]
+  );
+  return res.rows[0];
+}
+
+module.exports = {
+  createTicket,
+  getTicketByChannel
+};
