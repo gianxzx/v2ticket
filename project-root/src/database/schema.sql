b3f1c309d1760e413290a5ab969840aecbe444ad
gianxzx <tulanagian6@gmail.com>
2026-02-04 23:51:46 +0800

    Update schema.sql

--- project-root/src/database/schema.sql
+++ project-root/src/database/schema.sql
@@ -1,1 +1,9 @@
-
+CREATE TABLE IF NOT EXISTS servers (
+  guild_id BIGINT PRIMARY KEY,
+  chef_role_id BIGINT NOT NULL,
+  admin_role_id BIGINT NOT NULL,
+  ticket_category_id BIGINT NOT NULL,
+  transcript_channel_id BIGINT NOT NULL,
+  discount_channel_id BIGINT NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW()
+);
