0c8a1d09c28147e29689b794e1cec31dd21ef46d
gianxzx <tulanagian6@gmail.com>
2026-02-04 23:57:16 +0800

    Update createChannel.js

--- project-root/src/tickets/createChannel.js
+++ project-root/src/tickets/createChannel.js
@@ -1,1 +1,20 @@
+const buildPermissions = require("./permissions");
+const { getServerConfig } = require("../database/queries/servers");
 
+module.exports = async function createTicketChannel(interaction) {
+  const config = await getServerConfig(interaction.guild.id);
+  if (!config) throw new Error("Server not configured");
+
+  const channel = await interaction.guild.channels.create({
+    name: `ticket-${interaction.user.username}`.toLowerCase(),
+    type: 0, // GUILD_TEXT
+    parent: config.ticket_category_id,
+    permissionOverwrites: buildPermissions({
+      guild: interaction.guild,
+      customerId: interaction.user.id,
+      chefRoleId: config.chef_role_id
+    })
+  });
+
+  return channel;
+};
